885027da7c5000cf2b3f0f63d60b79f038d5077b
Andres <andres_erazo30@gmail.com>
2020-07-20 16:16:08 -0500

    actuliazacion hbs descuentos

--- models/product.js
+++ models/product.js
@@ -6,7 +6,9 @@ var schema = new Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
     price: {type: Number, required: true},
-    discount: {type: Number, required: true}
+    discount: {type: Number, required: true},
+    priceDiscount: {type: Number, required: true},
+    
 });
 
 module.exports = mongoose.model('Product', schema);
--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -15,42 +15,48 @@ var products = [
         title: 'Dry Poly Sports Tshirt',
         description: ' quick drying, plain coloured sports tee for adults',
         price: 19.900,
-        discount: 0 
+        discount: 0,
+        priceDiscount: 0 
     }),
     new Product({
         imagePath: 'https://ehosting.be/wp-content/uploads/2015/06/5.jpg',
         title: 'T-shirt 10',
         description: 'Large, Medium, Small, XL, XS',
         price: 19.900 ,
-        discount: 0
+        discount: 0,
+        priceDiscount: 0 
     }),
     new Product({
         imagePath: 'https://shirtsofcotton.com/en/media/catalog/product/cache/10/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/s/h/shirtsofcotton-t-shirt-soc-06-front-1200px.jpg',
         title: 'Grey V-Neck T-shirt',
         description: '95% quality cotton, 5% elastane',
         price: 19.900 ,
-        discount: 0
+        discount: 0,
+        priceDiscount: 0 
     }),
     new Product({
         imagePath: 'https://huckberry.imgix.net/spree/products/469614/original/mMKoUUfLXv_proof_nova_series_insulated_jacket_insulated-jackets_0_original.jpg?auto=format%2Ccompress&crop=top&fit=clip&cs=tinysrgb&w=600&ixlib=react-9.0.1&h=600&w=600',
         title: 'Insulated Jacket',
         description: ' Primaloft Gold synthetic insulation, 60g',
         price: 110.000,
-        discount: 0
+        discount: 0,
+        priceDiscount: 0 
     }),
     new Product({
         imagePath: 'https://cdn.shopify.com/s/files/1/0055/1242/products/EXP52BMR-ARM-0_2048x.jpg?v=1560205039',
         title: 'LIGHTWEIGHT BOMBER JACKET',
         description: 'jacket has water resistant 82gm 100% polyester fabric with a zip front closur',
         price: 90000,
-        discount: 0
+        discount: 0,
+        priceDiscount: 0 
     }),
     new Product({
         imagePath: 'https://wwws.dior.com/couture/ecommerce/media/catalog/product/cache/1/zoom_image_1/3000x2000/17f82f742ffe127f42dca9de82fb58b1/7/g/1570207502_943C449A4462_C989_E01_ZH.jpg',
         title: 'DOWN JACKET',
         description: 'Slip side pockets',
         price: 90.000,
-        discount: 15
+        discount: 15,
+        priceDiscount: 0 
     })
 ];
 
